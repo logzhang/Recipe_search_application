0e338afaa5a1a73e4aaea3a232f1dc6dccae46cc
Xu Zhang <44478659+logzhang@users.noreply.github.com>
2021-03-21 20:53:47 -0700

    code for application

---
+++ src/Recipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from "./recipe.module.css";
+
+
+const Recipe = ({ title, calaroies, image, ingredients }) => {
+    return (
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            <p>{calaroies}</p>
+            <img src={image} alt="" />
+        </div>
+    );
+}
+
+export default Recipe;
